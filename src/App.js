325a38048f1ece071b2f6ea06c85248a44d208f4
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-06-17 21:26:12 -0400

    stretch 1: 3

--- src/App.js
+++ src/App.js
@@ -30,18 +30,18 @@ function App() {
 
   return (
     <div className="container">
-      <section className="scoreboard">
+      <section className="scoreboard" style ={{backgroundColor:'hotpink', border: "dashed .2rem navy"}}>
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
+            <h2 className="home__name" style ={{backgroundColor:'cadetBlue', fontStyle: 'italic'}}>Lions</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these
             divs to accept dynamic values from our state. */}
             <div className="home__score">{homeNum}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer" style = {{backgroundColor: 'navy'}}>00:03</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
+            <h2 className="away__name" style ={{backgroundColor:'gold', fontStyle:'italic'}}>Tigers</h2>
             <div className="away__score">{awayNum}</div>
           </div>
         </div>
@@ -51,12 +51,12 @@ function App() {
         <div className="homeButtons">
 
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick = {()=> homeCount(homeNum +7)}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick = {()=> homeCount(homeNum + 3)}>Home Field Goal</button>
+          <button className="homeButtons__touchdown" style = {{backgroundColor: 'navy'}} onClick = {()=> homeCount(homeNum +7)}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" style = {{backgroundColor: 'navy'}} onClick = {()=> homeCount(homeNum + 3)}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick ={()=> awayCount(awayNum + 7)}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick ={()=> awayCount(awayNum + 3)}>Away Field Goal</button>
+          <button className="awayButtons__touchdown" style = {{backgroundColor: 'navy'}} onClick ={()=> awayCount(awayNum + 7)}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" style = {{backgroundColor: 'navy'}} onClick ={()=> awayCount(awayNum + 3)}>Away Field Goal</button>
         </div>
       </section>
     </div>
